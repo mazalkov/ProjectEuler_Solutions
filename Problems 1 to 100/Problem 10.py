c56f39ee6db3c2130df23568e16df37552cc00d4
Andrew <32670729+mazalkov@users.noreply.github.com>
2021-06-08 12:52:53 +0100

    Create Problem 10.py

---
+++ Problems 1 to 100/Problem 10.py
@@ -0,0 +1,22 @@
+# https://projecteuler.net/problem=10
+
+
+def eratosthenes(n):
+
+    multiples = set()
+
+    for i in range(2, n+1):
+
+        if i not in multiples:
+
+            yield i
+
+            multiples.update(range(i*i, n+1, i))
+
+
+iter = 0
+ml = list(eratosthenes(2000000))
+for x in ml:
+    iter = int(x) + iter
+
+print(iter)
